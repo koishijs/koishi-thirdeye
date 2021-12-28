e8af0be3c97593752dc7e937fd480d77cc61ca00
purerosefallen <78877@qq.com>
2021-12-28 09:36:01 +0800

    add unittest

--- tests/inner-plugin.spec.ts
+++ tests/inner-plugin.spec.ts
@@ -2,28 +2,30 @@ import { App } from 'koishi';
 import { DefinePlugin } from '../src/register';
 import { UsePlugin } from '../src/decorators';
 import { PluginDef } from '../src/def';
-import PluginLru from '@koishijs/plugin-cache-lru';
 import PluginOnebot from '@koishijs/plugin-adapter-onebot';
+import { BasePlugin } from '../src/base-plugin';
 
 @DefinePlugin()
-class MyPlugin {
-  @UsePlugin()
-  loadLru() {
-    return PluginDef(PluginLru);
-  }
-
+class MyPlugin extends BasePlugin<any> {
   @UsePlugin()
   loadOnebot() {
-    return PluginDef(PluginOnebot, { bots: [] });
+    return PluginDef(PluginOnebot, {
+      bots: [{ protocol: 'wsreverse', selfId: '11111' }],
+    });
   }
 }
 
 describe('Inner plugin', () => {
   let app: App;
-  beforeEach(() => {
-    app = new App();
+  beforeEach(async () => {
+    app = new App({ port: 11111 });
+    await app.start();
     // app.plugin(PluginOnebot, { bots: [] });
   });
 
-  it('should load inner plugin properly', () => {});
+  it('should load inner plugin properly', () => {
+    app.plugin(MyPlugin);
+    expect(app.bots.length).toBe(1);
+    //expect(app.cache).toBeDefined();
+  });
 });
