48ec97036a48e0862b50a05431077214f39a3019
purerosefallen <78877@qq.com>
2021-12-28 09:14:16 +0800

    update type

--- src/def/interfaces.ts
+++ src/def/interfaces.ts
@@ -22,27 +22,24 @@ export interface ContextSelector {
   useSelector?: OnContextFunction;
 }
 
-export type KoishiPluginOptions<T extends keyof Modules | Plugin> =
-  | boolean
-  | Plugin.Config<T>;
+export type KoishiPluginOptions<T extends Plugin> = boolean | Plugin.Config<T>;
 
-export interface KoishiModulePlugin<T extends keyof Modules | Plugin>
-  extends ContextSelector {
-  plugin: T;
-  options?: boolean | KoishiPluginOptions<T>;
+export interface KoishiModulePlugin<T extends Plugin> extends ContextSelector {
+  plugin: T | string;
+  options?: boolean | KoishiPluginOptions<T> | any;
 }
 
-export function PluginDef<T extends keyof Modules>(
-  plugin: T,
-  options?: boolean | Plugin.Config<T>,
+export function PluginDef<T extends keyof Plugin>(
+  name: string,
+  options?: any,
   select?: Selection,
-): KoishiModulePlugin<T>;
+);
 export function PluginDef<T extends Plugin>(
   plugin: T,
-  options?: boolean | Plugin.Config<T>,
+  options?: KoishiModulePlugin<T>,
   select?: Selection,
-): KoishiModulePlugin<T>;
-export function PluginDef<T extends keyof Modules | Plugin>(
+);
+export function PluginDef<T extends keyof Plugin>(
   plugin: T,
   options?: KoishiPluginOptions<T>,
   select?: Selection,
