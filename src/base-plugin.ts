14768a06349bd2a24f3ab9a2a6e7d4dc3e6034a2
purerosefallen <78877@qq.com>
2022-02-18 21:50:08 +0800

    catchup & allow another partial config type

--- src/base-plugin.ts
+++ src/base-plugin.ts
@@ -1,8 +1,8 @@
 import { Context } from 'koishi';
 import { InjectConfig } from './decorators';
 
-export class BasePlugin<C> {
-  constructor(protected ctx: Context, config: Partial<C>) {}
+export class BasePlugin<C, PC = Partial<C>> {
+  constructor(protected ctx: Context, config: PC) {}
 
   @InjectConfig()
   protected config: C;
