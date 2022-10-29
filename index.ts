9ffc735cf9ce9e4f9e34a3e73fa5f03e8639f595
purerosefallen <78877@qq.com>
2022-10-29 12:49:49 +0800

    workaround fix for selectContext again

--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
-import { Context } from 'koishi';
-
 export * from 'satori-decorators';
 export * from './src/cosmotype-exports';
 export * from './src/decorators';
 export * from './src/def';
 export * from './src/registrar';
-
-declare module 'satori-decorators' {
-  function selectContext<Ctx extends Context>(
-    root: Ctx,
-    options: Selection,
-  ): Ctx;
-}
+export * from './src/utility/select-context';
